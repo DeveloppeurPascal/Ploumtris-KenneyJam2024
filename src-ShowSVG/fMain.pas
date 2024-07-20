f83a409ea002a2398d20068818bfb5bdfc5085c6
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-20 13:44:24 +0200

    replaced previous display code by the new unit to get a bitmap from a SVG

--- src-ShowSVG/fMain.pas
+++ src-ShowSVG/fMain.pas
@@ -40,7 +40,8 @@ implementation
 uses
   System.TypInfo,
   PuzzleAssets2,
-  Olf.Skia.SVGToBitmap;
+  Olf.Skia.SVGToBitmap,
+  uSVGToImages;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -60,132 +61,27 @@ procedure TForm1.ShowAll;
 var
   bmp: tbitmap;
   img: timage;
-  i: integer;
+  SVGIndex: TSVGSVGIndex;
   lbl: TLabel;
-  MargeHaut, MargeBas, MargeGauche, MargeDroite: single;
-  BMPScale: single;
 begin
-  for i := 0 to length(SVGSVG) - 1 do
+  for SVGIndex := low(TSVGSVGIndex) to high(TSVGSVGIndex) do
   begin
-    // bmp := TOlfSVGBitmapList.Bitmap(i, round(GridLayout1.ItemWidth),
-    // round(GridLayout1.Itemheight));
-    // img := timage.Create(self);
-    // img.Bitmap.Assign(bmp);
-    // img.Parent := GridLayout1;
-    MargeHaut := 0;
-    MargeDroite := 0;
-    MargeBas := 0;
-    MargeGauche := 0;
-    case TSVGSVGIndex(i) of
-      TSVGSVGIndex.EauDb:
-        begin
-          MargeHaut := 100 * ((117.55 - 87.9) / 117.55);
-          MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.EauGb:
-        begin
-          MargeHaut := 100 * ((117.55 - 88.05) / 117.55);
-          MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-        end;
-      TSVGSVGIndex.EauGd:
-        begin
-          MargeHaut := 100 * ((117.55 - 58.6) / 117.55) / 2;
-          MargeDroite := 0;
-          MargeBas := 100 * ((117.55 - 58.6) / 117.55) / 2;
-          MargeGauche := 0;
-        end;
-      TSVGSVGIndex.EauGdb:
-        MargeHaut := 100 * ((117.55 - 88.05) / 117.55);
-      TSVGSVGIndex.EauGdh:
-        MargeBas := 100 * ((117.55 - 87.9) / 117.55);
-      TSVGSVGIndex.EauHb:
-        begin
-          MargeDroite := 100 * ((117.55 - 58.6) / 117.55) / 2;
-          MargeGauche := 100 * ((117.55 - 58.6) / 117.55) / 2;
-        end;
-      TSVGSVGIndex.EauHd:
-        begin
-          MargeBas := 100 * ((117.55 - 87.9) / 117.55);
-          MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.EauHdb:
-        MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-      TSVGSVGIndex.EauHg:
-        begin
-          MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-          MargeBas := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.EauHgb:
-        MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-      TSVGSVGIndex.PipeDb:
-        begin
-          MargeHaut := 100 * ((117.55 - 87.9) / 117.55);
-          MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.PipeGb:
-        begin
-          MargeHaut := 100 * ((117.55 - 88.05) / 117.55);
-          MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-        end;
-      TSVGSVGIndex.PipeGd:
-        begin
-          MargeHaut := 100 * ((117.55 - 58.6) / 117.55) / 2;
-          MargeBas := 100 * ((117.55 - 58.6) / 117.55) / 2;
-        end;
-      TSVGSVGIndex.PipeGdb:
-        MargeHaut := 100 * ((117.55 - 88.05) / 117.55);
-      TSVGSVGIndex.PipeHb:
-        begin
-          MargeDroite := 100 * ((117.55 - 58.6) / 117.55) / 2;
-          MargeGauche := 100 * ((117.55 - 58.6) / 117.55) / 2;
-        end;
-      TSVGSVGIndex.PipeHd:
-        begin
-          MargeBas := 100 * ((117.55 - 87.9) / 117.55);
-          MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.PipeHdb:
-        MargeGauche := 100 * ((117.55 - 88.05) / 117.55);
-      TSVGSVGIndex.PipeHg:
-        begin
-          MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-          MargeBas := 100 * ((117.55 - 88.05) / 117.55);
-        end;
-      TSVGSVGIndex.PipeHgb:
-        MargeDroite := 100 * ((117.55 - 87.9) / 117.55);
-      TSVGSVGIndex.PipeHgd:
-        MargeBas := 100 * ((117.55 - 87.9) / 117.55);
-    end;
-
     img := timage.Create(self);
     img.Parent := GridLayout1;
-
     img.WrapMode := TImageWrapMode.Original;
-    BMPScale := img.Bitmap.BitmapScale;
 
-    bmp := TOlfSVGBitmapList.Bitmap(i,
-      round(BMPScale * GridLayout1.ItemWidth * (100 - MargeGauche - MargeDroite)
-      / 100), round(BMPScale * GridLayout1.Itemheight * (100 - MargeHaut -
-      MargeBas) / 100));
-
-    img.Bitmap.SetSize(round(img.width * img.Bitmap.BitmapScale),
-      round(img.height * img.Bitmap.BitmapScale));
-    img.Bitmap.Clear(TAlphaColors.Snow);
-    // showmessage(img.bitmap.Width.tostring+','+img.bitmap.Height.tostring);exit;
-    img.Bitmap.Canvas.BeginScene;
+    bmp := getBitmapFromSVG(SVGIndex, img.width, img.height,
+      img.bitmap.BitmapScale, talphacolors.Yellow);
     try
-      img.Bitmap.Canvas.DrawBitmap(bmp, bmp.BoundsF,
-        trectf.Create((img.Bitmap.width * MargeGauche / 100) / BMPScale,
-        (img.Bitmap.height * MargeHaut / 100) / BMPScale,
-        (bmp.width + img.Bitmap.width * MargeGauche / 100) / BMPScale,
-        (bmp.height + img.Bitmap.height * MargeHaut / 100) / BMPScale), 1);
+      img.bitmap.Assign(bmp);
     finally
-      img.Bitmap.Canvas.EndScene;
+      bmp.free;
     end;
+
     lbl := TLabel.Create(self);
     lbl.Parent := img;
     lbl.Align := talignlayout.client;
-    lbl.Text := GetEnumName(TypeInfo(TSVGSVGIndex), i);
+    lbl.Text := GetEnumName(TypeInfo(TSVGSVGIndex), ord(SVGIndex));
     lbl.TextSettings.HorzAlign := TTextAlign.Center;
   end;
 end;
