1757e54368f8fba289e5c78783e96879a4fd8134
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-20 08:20:09 +0200

    added units to use a gamepad (#7)

--- src/Ploumtris.dpr
+++ src/Ploumtris.dpr
@@ -3,7 +3,14 @@ program Ploumtris;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fMain in 'fMain.pas' {Form1};
+  fMain in 'fMain.pas' {Form1},
+  uJoystickManager in '..\src-temp\uJoystickManager.pas',
+  Gamolf.FMX.Joystick in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.Joystick.pas',
+  Gamolf.RTL.Joystick.DirectInput.Win in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.DirectInput.Win.pas',
+  Gamolf.RTL.Joystick.Mac in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.Mac.pas',
+  Gamolf.RTL.Joystick in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.pas',
+  iOSapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\iOSapi.GameController.pas',
+  Macapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\Macapi.GameController.pas';
 
 {$R *.res}
 
