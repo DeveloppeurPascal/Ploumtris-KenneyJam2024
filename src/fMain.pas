9068d7cd3698e060e8889449bdd812fbfc51a89f
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-20 09:40:21 +0200

    added the AboutBox dialog and filled its containt

--- src/fMain.pas
+++ src/fMain.pas
@@ -3,22 +3,94 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  Olf.FMX.AboutDialog,
+  uDMLogo;
 
 type
-  TForm1 = class(TForm)
+  TfrmMain = class(TForm)
+    OlfAboutDialog1: TOlfAboutDialog;
+    procedure OlfAboutDialog1URLClick(const AURL: string);
+    procedure FormCreate(Sender: TObject);
   private
-    { Déclarations privées }
+
+  protected
+    procedure InitAboutDialogBox;
+    procedure InitMainFormCaption;
   public
-    { Déclarations publiques }
   end;
 
 var
-  Form1: TForm1;
+  frmMain: TfrmMain;
 
 implementation
 
 {$R *.fmx}
+
+uses
+  u_urlOpen;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  InitAboutDialogBox;
+  InitMainFormCaption;
+end;
+
+procedure TfrmMain.InitAboutDialogBox;
+begin
+  // TODO : traduire texte(s)
+  OlfAboutDialog1.Licence.Text :=
+    'This program is distributed as shareware. If you use it (especially for ' +
+    'commercial or income-generating purposes), please remember the author and '
+    + 'contribute to its development by purchasing a license.' + slinebreak +
+    slinebreak +
+    'This software is supplied as is, with or without bugs. No warranty is offered '
+    + 'as to its operation or the data processed. Make backups!';
+  OlfAboutDialog1.Description.Text :=
+    'A Tetris for plumbers who love video games without water leaks.' +
+    slinebreak + slinebreak + '*****************' + slinebreak +
+    '* Publisher info' + slinebreak + slinebreak +
+    'This application was developed by Patrick Prémartin.' + slinebreak +
+    slinebreak +
+    'It is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.'
+    + slinebreak + slinebreak + '****************' + slinebreak +
+    '* Personal data' + slinebreak + slinebreak +
+    'This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.'
+    + slinebreak + slinebreak + 'We have no knowledge of what you do with it.' +
+    slinebreak + slinebreak +
+    'No information about you is transmitted to us or to any third party.' +
+    slinebreak + slinebreak +
+    'We use no cookies, no tracking, no stats on your use of the application.' +
+    slinebreak + slinebreak + '***************' + slinebreak + '* User support'
+    + slinebreak + slinebreak +
+    'If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.'
+    + slinebreak + slinebreak + 'To find out more, visit ' +
+    OlfAboutDialog1.URL;
+end;
+
+procedure TfrmMain.InitMainFormCaption;
+begin
+{$IFDEF DEBUG}
+  caption := '[DEBUG] ';
+{$ELSE}
+  caption := '';
+{$ENDIF}
+  caption := caption + OlfAboutDialog1.Titre + ' v' +
+    OlfAboutDialog1.VersionNumero;
+end;
+
+procedure TfrmMain.OlfAboutDialog1URLClick(const AURL: string);
+begin
+  url_Open_In_Browser(AURL);
+end;
 
 end.
