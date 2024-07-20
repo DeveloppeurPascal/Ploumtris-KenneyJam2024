b895b726993ca20822e754f3fb0d9ea5f0f4805e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-20 19:50:56 +0200

    added aconstants unit

---
+++ src/uConsts.pas
@@ -0,0 +1,18 @@
+unit uConsts;
+
+interface
+
+Const
+  /// <summary>
+  /// Pipe's image size (in pixel)
+  /// </summary>
+  CPipeSize = 32;
+  /// <summary>
+  /// Size of the game zone
+  /// </summary>
+  CNbCol = 10;
+  CNBRow = 20;
+
+implementation
+
+end.
