e41971749b6032d260521b75474c67f874a73179
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-28 15:35:19 +0200

    updated units list for UIElements and GameController components (#88, #89)

--- src/Ploumtris.dpr
+++ src/Ploumtris.dpr
@@ -5,7 +5,6 @@ uses
   FMX.Forms,
   FMX.Skia,
   fMain in 'fMain.pas' {frmMain},
-  uJoystickManager in '..\src-temp\uJoystickManager.pas',
   Gamolf.FMX.Joystick in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.Joystick.pas',
   Gamolf.RTL.Joystick.DirectInput.Win in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.DirectInput.Win.pas',
   Gamolf.RTL.Joystick.Mac in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.Mac.pas',
@@ -13,7 +12,6 @@ uses
   iOSapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\iOSapi.GameController.pas',
   Macapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\Macapi.GameController.pas',
   Gamolf.FMX.MusicLoop in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.MusicLoop.pas',
-  uUIElements in '..\src-temp\uUIElements.pas',
   Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
   Gamolf.RTL.Scores in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Scores.pas',
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
@@ -32,7 +30,8 @@ uses
   uBackgroundMusic in 'uBackgroundMusic.pas',
   cDialogBox in 'cDialogBox.pas' {cadDialogBox: TFrame},
   cPloumtrisTitle in 'cPloumtrisTitle.pas' {cadPloumtrisTitle: TFrame},
-  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas';
+  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas',
+  Gamolf.RTL.UIElements in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.UIElements.pas';
 
 {$R *.res}
 
